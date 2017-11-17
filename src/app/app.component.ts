a1321d8ee46ec637f2647ef2f2f03b3b0651f2b5
Victor King'e <vinge1718@gmail.com>
2017-11-17 16:07:33 +0300

    add the code for displaying the list of meals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Meal Tracker';
+
+  public meals: Meal[] = [
+    new Meal("Breakfast.", 300, "coffee and toast"),
+    new Meal("Lunch.", 900, "pilau and two drum-sticks"),
+    new Meal("Dinner.", 1200, "Ugali, Mursik and Bacon")
+  ];
 }
